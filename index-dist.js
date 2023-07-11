b5bca5aa893308dce004e458225f8982221d1589
Gp3ShenQ <gp3g4xup6@gmail.com>
2023-07-12 00:59:24 +0800

    2023.07.12 v1

---
+++ index-dist.js
@@ -0,0 +1,1 @@
+$(".markBox").on("click",(function(){$("html,body").animate({scrollTop:0},333)}));
---
+++ index.js
@@ -0,0 +1,3 @@
+$('.markBox').on('click',function(){
+  $('html,body').animate({scrollTop:0}, 333)
+})
